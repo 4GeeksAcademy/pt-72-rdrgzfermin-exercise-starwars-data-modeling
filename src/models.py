f56bacdf833745f3d9e1759977d05fdd297b1761
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-01 23:58:18 +0000

    save

--- src/models.py
+++ src/models.py
@@ -14,18 +14,41 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
     def to_dict(self):
+        return {}
+    
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    height = Column(Integer)
+    mass = Column(Integer)
+    eye_color = Column(String(255))
+
+class Planet(Base):
+    __planet__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    terrain = Column(String(255))
+    diameter = Column(Integer)
+    population = Column(Integer)
+
+    def serialize(self):
+        return {}
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    character_id = Column(Integer, ForeignKey("character.id"))
+    
+    user = relationship("User", Column="favorites")
+    planet = relationship("Planet", Column="favorites")
+    character = relationship("Character", Column="favorites")
+
+    def serialize(self):
         return {}
 
 ## Draw from SQLAlchemy base
